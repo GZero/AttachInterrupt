aae24d8c8dd9575fbefa54fea8fd3398ac998dfc
GZero <43668274+GZero@users.noreply.github.com>
2019-04-28 12:32:21 +0800

    Add files via upload

---
+++ AttachInterrupt/AttachInterrupt.ino
@@ -0,0 +1,16 @@
+int led = PB1;  //http://docs.leaflabs.com/docs.leaflabs.com/index.html
+int but = PB8;
+volatile int bLED = LOW;
+
+void interruptFunction() {bLED = !bLED;}
+
+void setup() {
+   pinMode(led, OUTPUT);
+   pinMode(but,INPUT_PULLDOWN);
+   attachInterrupt(but, interruptFunction, CHANGE);
+}
+
+void loop() {
+  digitalWrite(led,bLED);
+}
+
